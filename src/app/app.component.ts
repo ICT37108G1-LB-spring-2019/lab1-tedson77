59fa5640df3a5b960f4621e7e5a48f28fb4a4578
tedson77 <44519319+tedson77@users.noreply.github.com>
2019-04-15 12:02:05 +0400

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'LAB 1 shalva-zaqaidze';
+  title = 'LAB 1 beqa tediashvili';
 }
